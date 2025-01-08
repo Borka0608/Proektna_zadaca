f5d4fd75c80288d2979ed4a3c450b4b2581e3e16
Borka0608 <bmicovska@gmail.com>
2025-01-08 21:55:44 +0100

    first commit

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print('hello')
+from flask import Flask, request, jsonify
+from extensions import db
+from models import Book, Author
+
+app = Flask(__name__)
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_vouchers.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database with the app
+db.init_app(app)
+
+# Create tables at startup
+with app.app_context():
+    db.create_all()
